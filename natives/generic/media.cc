11bf794c8ad339bb939d472f58e1312923c4d090
Samuel Hernandez <sam@samhza.com>
2026-09-07 21:40:19 -0400

    fix: Correct libvips version check

--- natives/generic/media.cc
+++ natives/generic/media.cc
@@ -58,7 +58,7 @@ void esmb_media_init() {
 #endif
   if (VIPS_INIT("")) vips_error_exit(NULL);
   vips_cache_set_max(0);
-#if VIPS_MAJOR_VERSION >= 8 && VIPS_MINOR_VERSION >= 13
+#if VIPS_MAJOR_VERSION > 8 || (VIPS_MAJOR_VERSION == 8 && VIPS_MINOR_VERSION >= 13)
   vips_block_untrusted_set(true);
   vips_operation_block_set("VipsForeignLoad", true);
   vips_operation_block_set("VipsForeignLoadJpeg", false);
